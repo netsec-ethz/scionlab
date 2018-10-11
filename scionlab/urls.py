f2138a19d59d7c6e3de6398d4a4d1562e58cc806
Matthias Frei <matthias.frei@inf.ethz.ch>
2018-10-11 16:35:32 +0200

    Import misc templates and static files from scion-coord
    Not cleaned, most of these will not work

--- scionlab/urls.py
+++ scionlab/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name= 'scionlab/login.html')),
     path('admin/', admin.site.urls),
 ]
