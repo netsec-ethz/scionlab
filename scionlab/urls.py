6498304c97d960e37389aa33e2fd2952a6fefc04
FR4NK-W <wirzf@inf.ethz.ch>
2018-10-24 14:44:52 +0200

    lint and fix test for generic form
    Rename activation to registration url

--- scionlab/urls.py
+++ scionlab/urls.py
@@ -28,15 +28,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Authentication
-    path('user/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    # django.contrib.auth built-in auth views for logout and password config actions like reset and change
+    path('user/login/',
+         auth_views.LoginView.as_view(template_name='registration/login.html'),
+         name='login'),
+    # django.contrib.auth: auth views for logout, password reset/change
     path('user/', include('django.contrib.auth.urls')),
+
     # user pages
     path('user/', login_required(ASesView.as_view(), login_url=reverse_lazy('login')), name='user'),
     path('user/test/', PlaceholderUserView.as_view(), name='userpage'),
 
     # django-registration patterns
-    path('activation/register/', UserRegistrationView.as_view(template_name='django_registration/registration_form.html'),
+    path('registration/register/',
+         UserRegistrationView.as_view(template_name='django_registration/registration_form.html'),
          name='registration_form'),
-    path('activation/', include('django_registration.backends.activation.urls')),
+    path('registration/', include('django_registration.backends.activation.urls')),
 ]
