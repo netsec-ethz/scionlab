8f86fd9daed4194b23f84e1469087f56bdaadf5b
FR4NK-W <wirzf@inf.ethz.ch>
2018-10-26 19:36:57 +0200

    Split ASes view into ASes overview and AS details

--- scionlab/views/as_detail_view.py
+++ scionlab/views/as_detail_view.py
@@ -15,24 +15,25 @@
 from django.conf import settings
 from django.views.generic import TemplateView
 
-from scionlab.models import AS, User
+from scionlab.models import AS, AttachmentPoint, User
 
 
-class ASesView(TemplateView):
-    template_name = "scionlab/user.html"
+class ASDetailView(TemplateView):
+    template_name = "scionlab/as_details.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         username = self.request.user
         current_user = User.objects.filter(username=username).first()
         ASes = AS.objects
-        user_ASes = ASes.filter(owner=current_user)
+        user_AS = ASes.filter(owner=current_user).filter(as_id=context['as_id']).first()
+        attachment_points = AttachmentPoint.objects.prefetch_related('AS').all()
+        attachment_points_ASes = [ap.AS for ap in attachment_points]
 
         # Context
         context['grafana_url'] = settings.GRAFANA_URL
-        context['username'] = username
-        context['as_count'] = len(user_ASes)
-        context['quota'] = settings.MAX_ASES_USER
-        context['user_ases'] = user_ASes
-        context['ases'] = user_ASes
+        context['user_as'] = user_AS
+        context['attachment_points'] = attachment_points_ASes
+        context['vpn_support_enabled'] = False
+        context['iot_images'] = settings.IOT_IMAGES
         return context
