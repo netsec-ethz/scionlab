cc2ff2df5dea2934f26e7493930ac453b220ba14
Matthias Frei <matthias.frei@inf.ethz.ch>
2019-05-06 12:12:39 +0200

    Keep secrets in env_file, collectstatics at startup

--- deploy/django-entrypoint.sh
+++ deploy/django-entrypoint.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+# Collect/update static files. These will be consumed by caddy, reading from the same volume.
+/scionlab/manage.py collectstatic --noinput
+
+# Wait for DB
 appdeps.py --wait-secs 60 --port-wait db:5432
 
+# Initialise/migrate DB
 /scionlab/manage.py makemigrations scionlab # TODO(matzf) remove once migrations are checked in
 /scionlab/manage.py migrate
+
 gunicorn -b django:8000 scionlab.wsgi
