ffd88245660b85996114dcce220b7c79e7f12fdc
FR4NK-W <wirzf@inf.ethz.ch>
2019-05-14 09:03:13 +0200

    Fix run dir locations

--- scionlab/tests/__init__.py
+++ scionlab/tests/__init__.py
@@ -8,8 +8,8 @@ def setup_package():
     app_dir = os.path.join(settings.BASE_DIR, "scionlab")
     dev_cert = "dev_root_ca_cert.pem"
     dev_key = "dev_root_ca_key.pem"
-    os.makedirs(os.path.join(app_dir, "run/"), exist_ok=True)
+    os.makedirs(os.path.join(settings.BASE_DIR, "run/"), exist_ok=True)
     shutil.copyfile(os.path.join(app_dir, "fixtures/", dev_cert),
-                    os.path.join(app_dir, "run/", dev_cert))
+                    os.path.join(settings.BASE_DIR, "run/", dev_cert))
     shutil.copyfile(os.path.join(app_dir, "fixtures/", dev_key),
-                    os.path.join(app_dir, "run/", dev_key))
+                    os.path.join(settings.BASE_DIR, "run/", dev_key))
