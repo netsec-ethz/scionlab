1a75b81d74eb4d5424828cf861fd5165fb63984d
Matthias Frei <matthias.frei@inf.ethz.ch>
2019-05-15 12:47:56 +0200

    Cleanup caddy image, use tags for postgres/redis images

--- deploy/django-entrypoint.sh
+++ deploy/django-entrypoint.sh
@@ -9,7 +9,6 @@ set -e
 appdeps.py --wait-secs 60 --port-wait db:5432
 
 # Initialise/migrate DB
-/scionlab/manage.py makemigrations scionlab # TODO(matzf) remove once migrations are checked in
 /scionlab/manage.py migrate
 
 gunicorn -b django:8000 scionlab.wsgi
