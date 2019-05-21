07d38d5b47fea12864a88b3f60c3508870a17c8d
Matthias Frei <matthias.frei@inf.ethz.ch>
2019-05-21 10:43:22 +0200

    Merge scionupgrade files from scion-coord

--- scionlab/hostfiles/scionupgrade.sh
+++ scionlab/hostfiles/scionupgrade.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-ACCOUNT_ID=$(<"$SC/gen/account_id")
-ACCOUNT_SECRET=$(<"$SC/gen/account_secret")
-IA=$(<"$SC/gen/ia")
+# SCION upgrade version 0.9
 
 wget https://raw.githubusercontent.com/netsec-ethz/scion-coord/master/scion_upgrade_script.sh -O upgrade.sh
 chmod +x upgrade.sh
 
-./upgrade.sh $ACCOUNT_ID $ACCOUNT_SECRET $IA
+./upgrade.sh
