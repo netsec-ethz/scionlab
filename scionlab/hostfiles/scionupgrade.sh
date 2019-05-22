1dbf348036c5b516c953500f69a50a7befb88791
Matthias Frei <matthias.frei@inf.ethz.ch>
2019-05-22 10:07:36 +0200

    Modify upgrade script WIP

--- scionlab/hostfiles/scionupgrade.sh
+++ scionlab/hostfiles/scionupgrade.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # SCION upgrade version 0.9
 
-wget https://raw.githubusercontent.com/netsec-ethz/scion-coord/master/scion_upgrade_script.sh -O upgrade.sh
+#wget https://raw.githubusercontent.com/netsec-ethz/scion-coord/master/scion_upgrade_script.sh -O upgrade.sh
+#wget https://testing.scionlab.org/static/scion_upgrade_script.sh -O upgrade.sh
+wget https://raw.githubusercontent.com/netsec-ethz/scionlab/scion-upgrade/scionlab/hostfiles/scion_upgrade_script.sh -O upgrade.sh
 chmod +x upgrade.sh
 
 ./upgrade.sh
