5482c3436fcb3b75994ba5a63773c793b306926b
Matthias Frei <matthias.frei@inf.ethz.ch>
2019-05-29 16:44:36 +0200

    Install scionlab-config with sudo

--- scionlab/hostfiles/scion_upgrade_script.sh
+++ scionlab/hostfiles/scion_upgrade_script.sh
@@ -109,7 +109,10 @@ MOTD1
 install_scionlab_config() {
     if [ -z `which scionlab-config` ]; then
         #wget https://testing.scionlab.org/static/scionlab-config -O /usr/bin/scionlab-config
-        wget "https://raw.githubusercontent.com/netsec-ethz/scionlab/scion-upgrade/scionlab/hostfiles/scionlab-config" -O /usr/local/bin/scionlab-config
+        tmpfile=`mktemp`
+        wget "https://raw.githubusercontent.com/netsec-ethz/scionlab/scion-upgrade/scionlab/hostfiles/scionlab-config" -O $tmpfile
+        chmod +x $tmpfile
+        sudo mv $tmpfile /usr/local/bin/scionlab-config
     fi
 }
 
