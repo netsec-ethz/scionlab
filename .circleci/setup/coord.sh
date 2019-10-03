20976ce24cb3cc13244b7feb571106985a53f5f8
Matthias Frei <matthias.frei@inf.ethz.ch>
2019-10-03 11:10:12 +0200

    Blah

--- .circleci/setup/coord.sh
+++ .circleci/setup/coord.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Regular coordinator installation procedure
-python3 -m venv /tmp/venv
-. /tmp/venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-pip install -r dev-requirements.txt
-
-# Load fixtures and initalize test DB
-PYTHONPATH=/tmp/scion/python scripts/init-test-db.sh
-
 # Start coordinator
-PYTHONPATH=/tmp/scion/python python manage.py runserver 0.0.0.0:8000
+PYTHONPATH=/tmp/scion/python 
 
