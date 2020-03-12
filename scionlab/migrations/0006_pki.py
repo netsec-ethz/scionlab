73222cb317ed64111e96119d6924dbc8acf8c321
Matthias Frei <matthias.frei@inf.ethz.ch>
2020-03-12 09:36:24 +0100

    Add migration to re-init PKI

--- scionlab/migrations/0006_pki.py
+++ scionlab/migrations/0006_pki.py
@@ -7,6 +7,39 @@ import jsonfield.fields
 import scionlab.models.pki
 
 
+def reset_pki(apps, schema_editor):
+    """
+    Previously, we had stored keys, certs and TRCs directly as string fields of the AS and ISD
+    objects. Now they are standalone objects, and these string fields have been removed.
+
+    Here we just create new keys and certs. There is no need to migrate any information, as the
+    previous CPPKI is incompatible anyway.
+    """
+    # Note: we use the _current_ model logic, not the "historical" migration models.
+    #
+    # From https://docs.djangoproject.com/en/2.2/topics/migrations/#historical-models
+    #  > If you import models directly rather than using the historical models, your migrations may
+    #  > work initially but will fail in the future when you try to re-run old migrations (commonly,
+    #  > when you set up a new installation and run through all the migrations to set up the DB).
+    #
+    # The logic for creating keys, TRCs and certificates is all implemented on models or model
+    # managers. Even though the steps for recreating the PKI from scratch are not overly complicated
+    # (it just requires to create things in the right order), it's complicated enough that I don't
+    # want a completely separate implementation on the "historical" models.
+    #
+    # As this code is a no-op on an empty database, I hope we avoid the trap described above.
+    # If you're reading this because this caused issues: sorry! Advice, from the same source:
+    #
+    #  > If you run into this kind of failure, it’s OK to edit the migration to use the historical
+    #  > models rather than direct imports and commit those changes.
+
+    from scionlab.models.core import AS, ISD
+    for as_ in AS.objects.all():
+        as_.init_keys()
+    for isd in ISD.objects.all():
+        isd.update_trc_and_certificates()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -118,4 +151,6 @@ class Migration(migrations.Migration):
                 'unique_together': {('AS', 'type', 'version')},
             },
         ),
+
+        migrations.RunPython(reset_pki),
     ]
