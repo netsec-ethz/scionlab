d3cfde5e0ad05f5843f90429dceaeee1f07279cf
Matthias Frei <matthias.frei@inf.ethz.ch>
2020-03-16 09:05:11 +0100

    Better list flattening

--- scionlab/models/pki.py
+++ scionlab/models/pki.py
@@ -30,6 +30,7 @@ from scionlab.defines import (
     DEFAULT_TRC_GRACE_PERIOD,
 )
 from scionlab.scion import as_ids, keys, trcs, certs
+from scionlab.util import flatten
 
 _MAX_LEN_CHOICES_DEFAULT = 16
 """ Max length value for choices fields without specific requirements to max length """
@@ -254,7 +255,7 @@ class TRCManager(models.Manager):
             prev_voting_offline = None
 
         keys = {as_.as_id: _latest_core_keys(as_) for as_ in isd.ases.filter(is_core=True)}
-        all_keys = sum(keys.values(), [])
+        all_keys = flatten(keys.values())
 
         not_before, not_after = _validity(all_keys)
 
