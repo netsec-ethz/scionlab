96fd0e09da990fcc2feaeed48b0a7a5323e6bcdb
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-11-05 22:10:54 +0100

    regeneration with better names

--- scionlab/tests/data/test_scion_trcs/regenerate.py
+++ scionlab/tests/data/test_scion_trcs/regenerate.py
@@ -23,70 +23,69 @@ from scionlab.scion.certs import _build_certificate, _build_extensions_as,\
                                  OID_SENSITIVE_KEY, OID_REGULAR_KEY
 
 
-def regenerate_voting_certs() -> None:
+def regenerate_voting_certs(asid) -> None:
+    fasid = asid.replace(":", "_")
     not_before = datetime(2020, 11, 12, 8, 0, tzinfo=timezone.utc)
     # sensitive:
     key = generate_key()
-    cert = _build_certificate(subject=(key, _create_name("1-ff00:0:110",
-                                                         "Sensitive Voting Certificate")),
+    cert = _build_certificate(subject=(
+        key, _create_name(f"1-{asid}", f"1-{asid} Sensitive Voting Certificate")),
                               issuer=None,
                               notvalidbefore=not_before,
                               notvalidafter=not_before + timedelta(days=1),
                               extensions=_build_extensions_voting(key, OID_SENSITIVE_KEY))
-    with open("voting-sensitive-1.key", "wb") as f:
+    with open(f"voting-sensitive-{fasid}.key", "wb") as f:
         f.write(encode_key(key).encode("ascii"))
-    with open("voting-sensitive-1.crt", "wb") as f:
+    with open(f"voting-sensitive-{fasid}.crt", "wb") as f:
         f.write(encode_certificate(cert).encode("ascii"))
     # regular:
     key = generate_key()
-    cert = _build_certificate(subject=(key, _create_name("1-ff00:0:110",
-                                                         "Regular Voting Certificate")),
+    cert = _build_certificate(subject=(
+        key, _create_name(f"1-{asid}", f"1-{asid} Regular Voting Certificate")),
                               issuer=None,
                               notvalidbefore=not_before,
                               notvalidafter=not_before + timedelta(days=1),
                               extensions=_build_extensions_voting(key, OID_REGULAR_KEY))
-    with open("voting-regular-1.key", "wb") as f:
+    with open(f"voting-regular-{fasid}.key", "wb") as f:
         f.write(encode_key(key).encode("ascii"))
-    with open("voting-regular-1.crt", "wb") as f:
+    with open(f"voting-regular-{fasid}.crt", "wb") as f:
         f.write(encode_certificate(cert).encode("ascii"))
 
 
-def regenerate_ca():
+def regenerate_ca(asid):
+    fasid = asid.replace(":", "_")
     not_before = datetime(2020, 11, 12, 8, 0, tzinfo=timezone.utc)
     # generate root:
     key = generate_key()
-    root_issuer = (key, _create_name("1-ff00:0:110",
-                                     "High Security Root Certificate"))
+    root_issuer = (key, _create_name(f"1-{asid}", f"1-{asid} High Security Root Certificate"))
     cert = _build_certificate(subject=root_issuer,
                               issuer=None,
                               notvalidbefore=not_before,
                               notvalidafter=not_before + timedelta(days=1),
                               extensions=_build_extensions_root(key))
-    with open("root-1.key", "wb") as f:
+    with open(f"root-{fasid}.key", "wb") as f:
         f.write(encode_key(key).encode("ascii"))
-    with open("root-1.crt", "wb") as f:
+    with open(f"root-{fasid}.crt", "wb") as f:
         f.write(encode_certificate(cert).encode("ascii"))
     # generate ca:
     key = generate_key()
-    ca_issuer = (key, _create_name("1-ff00:0:110",
-                                   "Secure CA Certificate"))
+    ca_issuer = (key, _create_name(f"1-{asid}", f"1-{asid} Secure CA Certificate"))
     cert = _build_certificate(subject=ca_issuer,
                               issuer=root_issuer,
                               notvalidbefore=not_before,
                               notvalidafter=not_before + timedelta(days=1),
                               extensions=_build_extensions_ca(key, root_issuer[0]))
-    with open("ca-1.key", "wb") as f:
+    with open(f"ca-{fasid}.key", "wb") as f:
         f.write(encode_key(key).encode("ascii"))
-    with open("ca-1.crt", "wb") as f:
+    with open(f"ca-{fasid}.crt", "wb") as f:
         f.write(encode_certificate(cert).encode("ascii"))
 
 
 def regenerate_ases():
     # get the CA key
-    with open("ca-1.key", "rb") as f:
+    with open(f"ca-ff00_0_110.key", "rb") as f:
         ca_key = decode_key(f.read().decode("ascii"))
-        issuer = (ca_key, _create_name("1-ff00:0:110",
-                                       "Secure CA Certificate"))
+        issuer = (ca_key, _create_name("1-ff00:0:110", "1-ff00:0:110 Secure CA Certificate"))
     # and generate
     for asid in ["1-ff00:0:110", "1-ff00:0:111", "1-ff00:0:112"]:
         key = generate_key()
@@ -95,9 +94,10 @@ def regenerate_ases():
                                   notvalidbefore=datetime.utcnow(),
                                   notvalidafter=datetime.utcnow() + timedelta(days=1),
                                   extensions=_build_extensions_as(key, issuer[0]))
-        with open(f"as{asid}.key", "wb") as f:
+        fasid = asid.replace(":", "_")
+        with open(f"as{fasid}.key", "wb") as f:
             f.write(encode_key(key).encode("ascii"))
-        with open(f"as{asid}.crt", "wb") as f:
+        with open(f"as{fasid}.crt", "wb") as f:
             f.write(encode_certificate(cert).encode("ascii"))
 
 
@@ -106,8 +106,8 @@ def regenerate_trc():
     _run_scion_cppki("payload", "-t", "payload-1-config.toml", "-o", "payload-1.der")
     # 2. sign payload with voters
     signers = [  # cert, key, outfile
-        ("voting-sensitive-1.crt", "voting-sensitive-1.key", "payload-1-signed-sensitive-1.der"),
-        ("voting-regular-1.crt", "voting-regular-1.key", "payload-1-signed-regular-1.der")]
+        ("voting-sensitive-ff00_0_110.crt", "voting-sensitive-ff00_0_110.key", "payload-1-signed-sensitive-ff00_0_110.der"),
+        ("voting-regular-ff00_0_110.crt", "voting-regular-ff00_0_110.key", "payload-1-signed-regular-ff00_0_110.der")]
     for (cert, key, outfile) in signers:
         command = ["openssl", "cms", "-sign", "-in", "payload-1.der",
                    "-inform", "der", "-md", "sha512", "-signer", cert,
@@ -121,13 +121,34 @@ def regenerate_trc():
     _run_scion_cppki("verify", "--anchor", "trc-1.trc", "trc-1.trc")
 
 
-def regenerate_updated_trc():
+def regenerate_regular_updated_trc():
     # 1. gen payload. Use payload-2-config.toml, that declares a regular update
     _run_scion_cppki("payload", "-t", "payload-2-config.toml", "-p", "trc-1.trc",
                      "-o", "payload-2.der")
     # 2. sign again with the regular certificate only
     signers = [  # cert, key, outfile
-        ("voting-regular-1.crt", "voting-regular-1.key", "payload-2-signed-regular-1.der")]
+        ("voting-regular-ff00_0_110.crt", "voting-regular-ff00_0_110.key", "payload-2-signed-regular-ff00_0_110.der")]
+    for (cert, key, outfile) in signers:
+        command = ["openssl", "cms", "-sign", "-in", "payload-2.der",
+                   "-inform", "der", "-md", "sha512", "-signer", cert,
+                   "-inkey", key, "-nodetach", "-nocerts", "-nosmimecap",
+                   "-binary", "-outform", "der", "-out", outfile]
+        subprocess.run(command, check=True)
+    # 3. combine signed payloads
+    _run_scion_cppki("combine", "-p", "payload-2.der", *(signed for (_, _, signed) in signers),
+                     "-o", "trc-2.trc")
+    # 4. verify TRC. This time it is anchored in the previous TRC.
+    _run_scion_cppki("verify", "--anchor", "trc-1.trc", "trc-2.trc")
+
+
+def regenerate_sensitive_update_trc():
+    """ it adds a new AS 1-ff00:0:210 as core, authoritative, and voter """
+    # 1. gen payload. Use payload-3-config.toml, that declares a sensitive update
+    _run_scion_cppki("payload", "-t", "payload-3-config.toml", "-p", "trc-2.trc",
+                     "-o", "payload-3.der")
+    # 2. sign again with the sensitive certificate only
+    signers = [  # cert, key, outfile
+        ("voting-regular-ff00_0_110.crt", "voting-regular-ff00_0_110.key", "payload-2-signed-regular-ff00_0_110.der")]
     for (cert, key, outfile) in signers:
         command = ["openssl", "cms", "-sign", "-in", "payload-2.der",
                    "-inform", "der", "-md", "sha512", "-signer", cert,
@@ -145,10 +166,14 @@ def regenerate():
     dir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir)
 
-    regenerate_voting_certs()
-    regenerate_ca()
+    regenerate_voting_certs("ff00:0:110")
+    regenerate_ca("ff00:0:110")
     regenerate_trc()
-    regenerate_updated_trc()
+    regenerate_regular_updated_trc()
+    
+    regenerate_voting_certs("ff00:0:210")
+    regenerate_ca("ff00:0:210")
+    regenerate_sensitive_update_trc()
 
 
 def _run_scion_cppki(*args):
