60cbf37b9a8935de95ceb11a8d23ccdfd359ac9b
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-11-25 16:22:56 +0100

    comment out for now import pkcs7

--- scionlab/scion/certs.py
+++ scionlab/scion/certs.py
@@ -33,7 +33,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 
-from pkcs7 import PKCS7Encoder
+# from pkcs7 import PKCS7Encoder
 from typing import cast, List, Tuple, NamedTuple, Optional
 
 
