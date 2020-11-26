netsec-ethz/scionlab

commit b0e96571c0cc436aa8316abdedfcff4e4c0f1fd1

Update generated config files to new format (#309)
Update generated config file to format in current scionproto/scion:master.
- `topology.json` has snake-case keys, simplified format for addresses. Bind address for control service was dropped. Split SIG ctrl/data addresses and add discovery service entry.
- logging to files is no longer supported, instead we just set the console loglevel; we'll use `journalctl` to inspect logs from now on
- small tweaks to `.toml` config files, in particular:
   -  CS for core ASes now has a `renewal_db`
   - `quic.resolution_fraction` is gone
- `sciond.toml` and `dispatcher.toml` are part of the packages
- `gen-certs` is gone
- command line flag changed from `-config` to `--config` & binary names updated in the supervisord config.

Note: this will fail in the integration tests until the CPPKI is updated too.

scionlab/scion/config.py
@@ -30,12 +30,10 @@ from scionlab.defines import (
     SD_TCP_PORT,
     SD_PROM_PORT,
     SCION_CONFIG_DIR,
-    SCION_LOG_DIR,
     SCION_VAR_DIR,
 )
 
 GEN = "gen"
-GEN_CERTS = "gen-certs"
 GEN_CACHE = "gen-cache"
 CERT_DIR = "certs"
 KEY_DIR = "keys"
@@ -57,9 +55,10 @@ BORDER_ENV = DEFAULT_ENV + ['GODEBUG="cgocheck=0"']
 
 CMDS = {
     Service.CS: 'cs',
-    TYPE_BR: 'border',
+    TYPE_BR: 'posix-router',
     TYPE_SD: 'sciond',
 }
+CMD_DISPATCHER = 'dispatcher'
 
 
 def generate_systemd_scion_config(host, archive, with_sig_dummy_entry=False):
@@ -108,9 +107,6 @@ class _ConfigGeneratorBase:
             assert service.type == Service.CS
             self.archive.write_toml((config_dir, f'{service.instance_name}.toml'),
                                     cb.build_cs_conf(service))
-
-        self.archive.write_toml((config_dir, 'sd.toml'),
-                                cb.build_sciond_conf(self.host))
 
         self._write_beacon_policy(config_dir, cb.build_beacon_policy(service))
         self._write_topo(config_dir)
@@ -165,11 +161,9 @@ class _ConfigGeneratorSystemd(_ConfigGeneratorBase):
 
     def generate(self):
         config_builder = _ConfigBuilder(config_dir=SCION_CONFIG_DIR,
-                                        log_dir=SCION_LOG_DIR,
-                                        var_dir=SCION_VAR_DIR,
-                                        tls_certs_dir=os.path.join(SCION_CONFIG_DIR, GEN_CERTS))
+                                        var_dir=SCION_VAR_DIR)
         self._write_as_config(config_builder)
-        # dispatcher config is installed with the package
+        # dispatcher and sciond config files are installed with the package
 
     def systemd_units(self):
         units = ["scion-border-router@%s.service" % router.instance_name
@@ -179,7 +173,7 @@ class _ConfigGeneratorSystemd(_ConfigGeneratorBase):
         # XXX(matzf) drop this!
         units += ["%s.service" % SERVICES_TO_SYSTEMD_NAMES[service.type]
                   for service in self._extra_services()]
-        units.append('scion-daemon@sd.service')
+        units.append('scion-daemon.service')
         units.append('scion-dispatcher.service')
         return units
 
@@ -191,21 +185,22 @@ class _ConfigGeneratorSupervisord(_ConfigGeneratorBase):
     def generate(self):
         # build AS service config into gen/AS<AS_ID>
         config_builder = _ConfigBuilder(config_dir=self._as_dir(),
-                                        log_dir='logs',
-                                        var_dir=GEN_CACHE,
-                                        tls_certs_dir=GEN_CERTS)
+                                        var_dir=GEN_CACHE)
         self._write_as_config(config_builder)
 
         # the dispatcher directory is outside the AS subdirectory
         self.archive.write_toml((self._disp_dir(), 'disp.toml'),
                                 config_builder.build_disp_conf())
 
+        self.archive.write_toml((self._as_dir(), 'sd.toml'),
+                                config_builder.build_sciond_conf(self.host))
+
         self._write_supervisord_file()
 
     def _write_supervisord_file(self):
         def _cmd(binary_name, config_dir, config_file):
             """ helper to create the command string for BR, CS, sciond and dispatcher """
-            return f'bin/{binary_name} -config {config_dir}/{config_file}'
+            return f'bin/{binary_name} --config {config_dir}/{config_file}'
 
         as_dir = self._as_dir()
         config = configparser.ConfigParser()
@@ -241,7 +236,7 @@ class _ConfigGeneratorSupervisord(_ConfigGeneratorBase):
         _add_supervisord_program_conf(
             config,
             disp_prog_id,
-            _cmd('godispatcher', self._disp_dir(), 'disp.toml'),
+            _cmd(CMD_DISPATCHER, self._disp_dir(), 'disp.toml'),
             env=DEFAULT_ENV,
             priority=50,
             startsecs=1
@@ -261,13 +256,13 @@ class _ConfigBuilder:
     Helper object for `_ConfigGenerator`
     Builds the *.toml-configuration for the SCION services.
     """
-    def __init__(self, config_dir, log_dir, var_dir, tls_certs_dir):
+    def __init__(self, config_dir, var_dir):
         self.config_dir = config_dir
-        self.log_dir = log_dir
         self.var_dir = var_dir
-        self.tls_certs_dir = tls_certs_dir
 
     def build_disp_conf(self):
+        # Note: this is only used in the supervisord setup;
+        # in the systemd setup, the dispatcher.toml file is installed with the package.
         logging_conf = self._build_logging_conf('dispatcher')
         metrics_conf = self._build_metrics_conf(DISPATCHER_PROM_PORT)
         conf = _chain_dicts(logging_conf, metrics_conf)
@@ -301,47 +296,47 @@ class _ConfigBuilder:
                 'rev_ttl': '20s',
                 'rev_overlap': '5s',
                 'policies': {
-                    'Propagation': os.path.join(self.config_dir, 'beacon_policy.yaml')
+                    'propagation': os.path.join(self.config_dir, 'beacon_policy.yaml')
                 }
             },
             'path_db': {
-                'backend': 'sqlite',
                 'connection': '%s.path.db' % os.path.join(self.var_dir, service.instance_name),
             },
             'beacon_db': {
-                'backend': 'sqlite',
                 'connection': '%s.beacon.db' % os.path.join(self.var_dir, service.instance_name),
             },
             'trust_db': {
-                'backend': 'sqlite',
                 'connection': '%s.trust.db' % os.path.join(self.var_dir, service.instance_name),
             },
             'quic': {
                 'address': _join_host_port(service.host.internal_ip, CS_QUIC_PORT),
-                'key_file':  os.path.join(self.tls_certs_dir, 'tls.key'),
-                'cert_file': os.path.join(self.tls_certs_dir, 'tls.pem'),
-                'resolution_fraction': 0.4,
             },
         })
+        if service.AS.is_core:
+            conf.update({
+                'renewal_db': {
+                    'connection': '%s.renewal.db' % os.path.join(self.var_dir,
+                                                                 service.instance_name),
+                },
+            })
+
         return conf
 
     def build_sciond_conf(self, host):
+        # Note: this is only used in the supervisord setup;
+        # in the systemd setup, the sciond.toml file is installed with the package.
         instance_name = 'sd'
-
         general_conf = self._build_general_conf(instance_name)
         logging_conf = self._build_logging_conf(instance_name)
         metrics_conf = self._build_metrics_conf(SD_PROM_PORT)
         conf = _chain_dicts(general_conf, logging_conf, metrics_conf)
         conf.update({
-            'sd': {
-                'address': _join_host_port('127.0.0.1', SD_TCP_PORT),
-            },
+            # Note, using default address:
+            # sd.address = 127.0.0.1:30255 (SD_TCP_PORT)
             'path_db': {
-                'backend': 'sqlite',
                 'connection': '%s.path.db' % os.path.join(self.var_dir, instance_name),
             },
             'trust_db': {
-                'backend': 'sqlite',
                 'connection': '%s.trust.db' % os.path.join(self.var_dir, instance_name),
             },
         })
@@ -362,6 +357,7 @@ class _ConfigBuilder:
                 # Note: this has performance impacts (for BR, only control plane)
                 'reconnect_to_dispatcher': True,
             },
+            # XXX: enable header v2 in features? or better flip the default in our builds?
         }
 
     def _build_metrics_conf(self, prometheus_port):
@@ -377,11 +373,8 @@ class _ConfigBuilder:
         """ Builds the 'logging' configuration section common to all services """
         return {
             'log': {
-                'file': {
-                    'path': '%s.log' % os.path.join(self.log_dir, instance_name),
+                'console': {
                     'level': 'debug',
-                    'max_age': 3,
-                    'max_backups': 1,
                 },
             },
         }

scionlab/defines.py
@@ -35,7 +35,8 @@ SD_TCP_PORT = 30255
 SD_PROM_PORT = 30455
 DISPATCHER_PORT = 30041
 DISPATCHER_PROM_PORT = 30441
-SIG_PORT = 31056
+SIG_CTRL_PORT = 30256
+SIG_DATA_PORT = 30056
 
 BW_PORT = 40001
 PP_PORT = 40002

scionlab/scion/topology.py
@@ -12,13 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import ipaddress
-
-from scionlab.defines import SIG_PORT
+from scionlab.defines import SIG_CTRL_PORT, SIG_DATA_PORT
 from scionlab.models.core import Service, Link
 
-KEY_BR = 'BorderRouters'
-KEY_CS = 'ControlService'
+KEY_BR = 'border_routers'
+KEY_CS = 'control_service'
+KEY_DS = 'discovery_service'
 
 TYPES_TO_KEYS = {
     "CS": KEY_CS,
@@ -40,7 +39,6 @@ class TopologyInfo:
     scionlab.models.core.BorderRouter/Service objects, marked up with the additional attributes
         `instance_name`
         `instance_id`
-
     """
 
     def __init__(self, as_, with_sig_dummy_entry=False):
@@ -51,22 +49,17 @@ class TopologyInfo:
         self.routers = _fetch_routers(self.AS)
         self.services = _fetch_services(self.AS)
 
-        # get overlay type inside AS from IPs used by hosts
-        address_type = _get_ip_type_str(self.AS.hosts.first().internal_ip)
-        overlay_type = "UDP/" + address_type
-
         # AS wide entries
         self.topo = {}
-        self.topo["ISD_AS"] = self.AS.isd_as_str()
-        self.topo["MTU"] = self.AS.mtu
-        self.topo["Attributes"] = CORE_ATTRIBUTES if self.AS.is_core else []
-        self.topo["Overlay"] = overlay_type
+        self.topo["isd_as"] = self.AS.isd_as_str()
+        self.topo["mtu"] = self.AS.mtu
+        self.topo["attributes"] = CORE_ATTRIBUTES if self.AS.is_core else []
 
-        _topo_add_routers(self.topo, self.routers, address_type)
-        _topo_add_control_services(self.topo, self.services, address_type)
+        _topo_add_routers(self.topo, self.routers)
+        _topo_add_control_services(self.topo, self.services)
 
         if with_sig_dummy_entry:
-            _topo_add_sig_dummy_entry(self.topo, self.AS.hosts.first(), address_type)
+            _topo_add_sig_dummy_entry(self.topo, self.AS.hosts.first())
 
 
 def _fetch_routers(as_):
@@ -89,7 +82,7 @@ def _fetch_services(as_):
     return services
 
 
-def _topo_add_routers(topo_dict, routers, as_address_type):
+def _topo_add_routers(topo_dict, routers):
     """
     Add entries for border routers to topology dict
     """
@@ -100,31 +93,17 @@ def _topo_add_routers(topo_dict, routers, as_address_type):
         if not interfaces:
             continue
 
-        router_entry = _topo_add_router_entry(topo_dict, router, as_address_type)
+        router_entry = _topo_add_router_entry(topo_dict, router)
 
         for interface in interfaces:
             _topo_add_interface(router_entry, interface)
 
 
-def _topo_add_router_entry(topo_dict, router, as_address_type):
+def _topo_add_router_entry(topo_dict, router):
     return topo_dict[KEY_BR].setdefault(router.instance_name, {
-        "InternalAddrs": {
-            as_address_type: {
-                "PublicOverlay": {
-                    "Addr": router.host.internal_ip,
-                    "OverlayPort": router.internal_port
-                }
-            }
-        },
-        "CtrlAddr": {
-            as_address_type: {
-                "Public": {
-                    "Addr": router.host.internal_ip,
-                    "L4Port": router.control_port
-                }
-            }
-        },
-        "Interfaces": {}
+        "internal_addr": _join_host_port(router.host.internal_ip, router.internal_port),
+        "ctrl_addr": _join_host_port(router.host.internal_ip, router.control_port),
+        "interfaces": {},
     })
 
 
@@ -134,63 +113,42 @@ def _topo_add_interface(router_entry, interface):
     """
     remote_interface = interface.remote_interface()
 
-    address_type = _get_ip_type_str(interface.get_public_ip())
-    link_overlay = "UDP/" + address_type
-
     interface_entry = {
-        "ISD_AS": remote_interface.AS.isd_as_str(),
-        "LinkTo": _get_linkto_relation(interface),
-        "Bandwidth": interface.link().bandwidth,
-        "PublicOverlay": {
-            "Addr": interface.get_public_ip(),
-            "OverlayPort": interface.public_port
+        "isd_as": remote_interface.AS.isd_as_str(),
+        "link_to": _get_linkto_relation(interface),
+        "mtu": interface.link().mtu,
+        "bandwidth": interface.link().bandwidth,
+        "underlay": {
+            "public": _join_host_port(interface.get_public_ip(), interface.public_port),
+            "remote": _join_host_port(remote_interface.get_public_ip(),
+                                      remote_interface.public_port),
         },
-        "MTU": interface.link().mtu,
-        "RemoteOverlay": {
-            "Addr": remote_interface.get_public_ip(),
-            "OverlayPort": remote_interface.public_port
-        },
-        "Overlay": link_overlay
     }
     if interface.get_bind_ip():
-        interface_entry.update({
-            "BindOverlay": {
-                "Addr": interface.get_bind_ip(),
-                "OverlayPort": interface.bind_port or interface.public_port
-            }
+        interface_entry["underlay"].update({
+            "bind": _join_host_port(interface.get_bind_ip(),
+                                    interface.bind_port or interface.public_port)
         })
-    router_entry["Interfaces"][interface.interface_id] = interface_entry
+    router_entry["interfaces"][interface.interface_id] = interface_entry
 
 
-def _topo_add_control_services(topo_dict, services, as_address_type):
+def _topo_add_control_services(topo_dict, services):
     control_services = (s for s in services if s.type in Service.CONTROL_SERVICE_TYPES)
     for service in control_services:
+        service_instance_entry = {
+            "addr": _join_host_port(service.host.internal_ip, service.port()),
+        }
         service_type_entry = topo_dict.setdefault(TYPES_TO_KEYS[service.type], {})
-        addrs = {
-            "Public": {
-                "Addr": service.host.internal_ip,
-                "L4Port": service.port()
-            }
-        }
-        # XXX(matzf): use optional bind_ip, but only if the internal_ip matches public_ip;
-        #   the reason is that the bind_ip is only really defined for the public_ip. If the
-        #   internal_ip is different from public_ip, e.g. if it's 127.0.0.1, then it doesn't make
-        #   sense to use bind_ip.
-        #   We'd need an internal_bind_ip to represent this correctly -- luckily we don't seem to
-        #   need this.
-        if service.host.bind_ip and service.host.internal_ip == service.host.public_ip:
-            addrs["Bind"] = {
-                "Addr": service.host.bind_ip,
-                "L4Port": service.port(),
-            }
-        service_type_entry[service.instance_name] = {
-            "Addrs": {
-                as_address_type: addrs
-            }
-        }
+        service_type_entry[service.instance_name] = service_instance_entry
+        # Add discovery service, as copy of control service (because the control service implements
+        # the discovery service, but there is still a separate entry in the topology file. Gee...).
+        if service.type == Service.CS:
+            ds_entry = topo_dict.setdefault(KEY_DS, {})
+            instance_name = f"ds-{service.instance_id}"
+            ds_entry[instance_name] = service_instance_entry
 
 
-def _topo_add_sig_dummy_entry(topo_dict, host, as_address_type):
+def _topo_add_sig_dummy_entry(topo_dict, host):
     """
     Add a "dummy" entry for the SIG in the topology.json, with localhost address and default
     port.
@@ -200,37 +158,26 @@ def _topo_add_sig_dummy_entry(topo_dict, host, as_address_type):
     Note: this entry allows the border router to resolve SIG service address; this changes the
     error behaviour slighly when receiving packets addressed to the SIG without the SIG running.
     """
-    topo_dict["SIG"] = {
+    topo_dict["sigs"] = {
         "sig-1": {  # id is irrelevant, not used for anything
-            "Addrs": {
-                as_address_type: {
-                    "Public": {
-                        "Addr": host.internal_ip,
-                        "L4Port": SIG_PORT,
-                    }
-                }
-            }
+            "ctrl_addr": _join_host_port(host.internal_ip, SIG_CTRL_PORT),
+            "data_addr": _join_host_port(host.internal_ip, SIG_DATA_PORT),
+            # 'allow_interfaces': [...], unused for now, not setting anything
         }
     }
 
 
-def _get_ip_type_str(ip):
+def _join_host_port(host, port):
     """
-    Return the type of the given IP address as string to be used in the scion topology
-    files; returns either "IPv4" or "IPv6"
-    :param str ip: IP address
-    :return str: "IPv4" or "IPv6"
+    Returns joined host:port, wrapping host in brackets if it looks like an IPv6 address
+    :param str host:
+    :param int port:
+        :return str:
     """
-    ip = ipaddress.ip_address(ip)
-    if isinstance(ip, ipaddress.IPv6Address):
-        return "IPv6"
+    if ':' in host:
+        return '[%s]:%s' % (host, port)
     else:
-        return "IPv4"
-
-
-def _get_link_overlay_type_str(interface):
-    ip_type = _get_ip_type_str(interface.get_public_ip())
-    return "UDP/" + ip_type
+        return '%s:%s' % (host, port)
 
 
 def _get_linkto_relation(interface):

scionlab/tests/data/test_config_tar/host_4.yml
@@ -139,11 +139,8 @@ etc/scion/br-1.toml: |
   [metrics]
   prometheus = "127.0.0.1:30442"
 
-  [log.file]
+  [log.console]
   level = "debug"
-  max_age = 3
-  max_backups = 1
-  path = "/var/log/scion/br-1.log"
 etc/scion/br-2.toml: |
   [general]
   config_dir = "/etc/scion"
@@ -153,11 +150,8 @@ etc/scion/br-2.toml: |
   [metrics]
   prometheus = "127.0.0.1:30443"
 
-  [log.file]
+  [log.console]
   level = "debug"
-  max_age = 3
-  max_backups = 1
-  path = "/var/log/scion/br-2.log"
 etc/scion/certs/ISD17-ASffaa_0_1107-V1.crt: |-
   [
     {
@@ -251,7 +245,6 @@ etc/scion/certs/ISD20-V1.trc: |-
   }
 etc/scion/cs-1.toml: |
   [beacon_db]
-  backend = "sqlite"
   connection = "/var/lib/scion/cs-1.beacon.db"
 
   [beaconing]
@@ -269,27 +262,19 @@ etc/scion/cs-1.toml: |
   prometheus = "127.0.0.1:30454"
 
   [path_db]
-  backend = "sqlite"
   connection = "/var/lib/scion/cs-1.path.db"
 
   [quic]
   address = "127.0.0.1:30354"
-  cert_file = "/etc/scion/gen-certs/tls.pem"
-  key_file = "/etc/scion/gen-certs/tls.key"
-  resolution_fraction = 0.4
 
   [trust_db]
-  backend = "sqlite"
   connection = "/var/lib/scion/cs-1.trust.db"
 
   [beaconing.policies]
-  Propagation = "/etc/scion/beacon_policy.yaml"
+  propagation = "/etc/scion/beacon_policy.yaml"
 
-  [log.file]
+  [log.console]
   level = "debug"
-  max_age = 3
-  max_backups = 1
-  path = "/var/log/scion/cs-1.log"
 etc/scion/keys/as-decrypt-v1.key: |
   -----BEGIN PRIVATE KEY-----
   algorithm: curve25519xsalsa20poly1305
@@ -316,136 +301,65 @@ etc/scion/keys/master0.key: |-
   R7D6Lhl52uxloBQFRulzzA==
 etc/scion/keys/master1.key: |-
   R7D6Lhl52uxloBQFRulzzA==
-etc/scion/sd.toml: |
-  [general]
-  config_dir = "/etc/scion"
-  id = "sd"
-  reconnect_to_dispatcher = true
-
-  [metrics]
-  prometheus = "127.0.0.1:30455"
-
-  [path_db]
-  backend = "sqlite"
-  connection = "/var/lib/scion/sd.path.db"
-
-  [sd]
-  address = "127.0.0.1:30255"
-
-  [trust_db]
-  backend = "sqlite"
-  connection = "/var/lib/scion/sd.trust.db"
-
-  [log.file]
-  level = "debug"
-  max_age = 3
-  max_backups = 1
-  path = "/var/log/scion/sd.log"
 etc/scion/topology.json: |-
   {
-    "Attributes": [],
-    "BorderRouters": {
+    "attributes": [],
+    "border_routers": {
       "br-1": {
-        "CtrlAddr": {
-          "IPv4": {
-            "Public": {
-              "Addr": "127.0.0.1",
-              "L4Port": 30242
-            }
-          }
-        },
-        "Interfaces": {
+        "ctrl_addr": "127.0.0.1:30242",
+        "interfaces": {
           "1": {
-            "Bandwidth": 1000,
-            "ISD_AS": "17-ffaa:0:1102",
-            "LinkTo": "PARENT",
-            "MTU": 1472,
-            "Overlay": "UDP/IPv4",
-            "PublicOverlay": {
-              "Addr": "192.0.2.17",
-              "OverlayPort": 50000
-            },
-            "RemoteOverlay": {
-              "Addr": "192.0.2.12",
-              "OverlayPort": 50001
+            "bandwidth": 1000,
+            "isd_as": "17-ffaa:0:1102",
+            "link_to": "PARENT",
+            "mtu": 1472,
+            "underlay": {
+              "public": "192.0.2.17:50000",
+              "remote": "192.0.2.12:50001"
             }
           }
         },
-        "InternalAddrs": {
-          "IPv4": {
-            "PublicOverlay": {
-              "Addr": "127.0.0.1",
-              "OverlayPort": 30042
-            }
-          }
-        }
+        "internal_addr": "127.0.0.1:30042"
       },
       "br-2": {
-        "CtrlAddr": {
-          "IPv4": {
-            "Public": {
-              "Addr": "127.0.0.1",
-              "L4Port": 30243
-            }
-          }
-        },
-        "Interfaces": {
+        "ctrl_addr": "127.0.0.1:30243",
+        "interfaces": {
           "2": {
-            "Bandwidth": 1000,
-            "ISD_AS": "17-ffaa:1:1",
-            "LinkTo": "CHILD",
-            "MTU": 1472,
-            "Overlay": "UDP/IPv4",
-            "PublicOverlay": {
-              "Addr": "10.0.8.1",
-              "OverlayPort": 50000
-            },
-            "RemoteOverlay": {
-              "Addr": "10.0.0.1",
-              "OverlayPort": 54321
+            "bandwidth": 1000,
+            "isd_as": "17-ffaa:1:1",
+            "link_to": "CHILD",
+            "mtu": 1472,
+            "underlay": {
+              "public": "10.0.8.1:50000",
+              "remote": "10.0.0.1:54321"
             }
           },
           "3": {
-            "Bandwidth": 1000,
-            "ISD_AS": "17-ffaa:1:5",
-            "LinkTo": "CHILD",
-            "MTU": 1472,
-            "Overlay": "UDP/IPv4",
-            "PublicOverlay": {
-              "Addr": "192.0.2.17",
-              "OverlayPort": 50001
-            },
-            "RemoteOverlay": {
-              "Addr": "172.31.0.200",
-              "OverlayPort": 54321
+            "bandwidth": 1000,
+            "isd_as": "17-ffaa:1:5",
+            "link_to": "CHILD",
+            "mtu": 1472,
+            "underlay": {
+              "public": "192.0.2.17:50001",
+              "remote": "172.31.0.200:54321"
             }
           }
         },
-        "InternalAddrs": {
-          "IPv4": {
-            "PublicOverlay": {
-              "Addr": "127.0.0.1",
-              "OverlayPort": 30043
-            }
-          }
-        }
+        "internal_addr": "127.0.0.1:30043"
       }
     },
-    "ControlService": {
+    "control_service": {
       "cs-1": {
-        "Addrs": {
-          "IPv4": {
-            "Public": {
-              "Addr": "127.0.0.1",
-              "L4Port": 30254
-            }
-          }
-        }
+        "addr": "127.0.0.1:30254"
       }
     },
-    "ISD_AS": "17-ffaa:0:1107",
-    "MTU": 1472,
-    "Overlay": "UDP/IPv4"
+    "discovery_service": {
+      "ds-1": {
+        "addr": "127.0.0.1:30254"
+      }
+    },
+    "isd_as": "17-ffaa:0:1107",
+    "mtu": 1472
   }
 scionlab-config.json: |-
   {
@@ -453,19 +367,18 @@ scionlab-config.json: |-
       "etc/openvpn/ccd/exbert@scionlab.org_ffaa_1_1": "72a44b475588d54d168220d99cb1d5b26a39decd",
       "etc/openvpn/server.conf": "94b139e770cf95e62b57596c2b363c9af4ed4ff8",
       "etc/scion/beacon_policy.yaml": "90c69ea879ca52546774c3007e7d29104766fc07",
-      "etc/scion/br-1.toml": "2df19de492ec1257d6cf3a244ead625c3fe3e102",
-      "etc/scion/br-2.toml": "7fd1b53d6c64cd2eff87c6c70d91990eb8909bcc",
+      "etc/scion/br-1.toml": "176ed3483535e5fa6b5bee83a786e978ae4ec081",
+      "etc/scion/br-2.toml": "34e0d88c394f36115029ac19c40399806c95ef97",
       "etc/scion/certs/ISD17-ASffaa_0_1107-V1.crt": "89bb64b0a92a9efc6d84e47ce1b2ab9c98bccf47",
       "etc/scion/certs/ISD17-V1.trc": "ed4f99c371b544e3bd6b4293e14396fd40b0b657",
       "etc/scion/certs/ISD19-V1.trc": "087f805b006e0ff52e8f4fbab3dcd63d27498601",
       "etc/scion/certs/ISD20-V1.trc": "df94e1688f2c88dd9648fa4c003f895b546467ab",
-      "etc/scion/cs-1.toml": "0c31ad123b489e233f616393faad56d4a7e93b5d",
+      "etc/scion/cs-1.toml": "0ef56839347bcb4cdef6c814ff2e1af86cbe8c89",
       "etc/scion/keys/as-decrypt-v1.key": "70b7a82e6d8ebbff8018b4f2deb251c308ac2e3b",
       "etc/scion/keys/as-signing-v1.key": "d79601ba79eea4161cc6b3f074f9d9e1df32bc5f",
       "etc/scion/keys/master0.key": "d3061b379b7044f794e598e1910f3a95dac95040",
       "etc/scion/keys/master1.key": "d3061b379b7044f794e598e1910f3a95dac95040",
-      "etc/scion/sd.toml": "6094e9c83d36e8ff82a188880b7612d4fb0f5c07",
-      "etc/scion/topology.json": "cb3d7a4480ec834dcf53201465034af672826b1c"
+      "etc/scion/topology.json": "5a642776686c4167e4e57726cc5de7fa902699c8"
     },
     "host_id": "9e090a4529264b958a5c65fd8c214b1d",
     "host_secret": "7abb3def0c4e4ddb85ba124d67d5544c",
@@ -474,7 +387,7 @@ scionlab-config.json: |-
       "scion-border-router@br-2.service",
       "scion-control-service@cs-1.service",
       "scion-bwtestserver.service",
-      "scion-daemon@sd.service",
+      "scion-daemon.service",
       "scion-dispatcher.service"
     ],
     "url": "http://localhost:8000",

commit e4f740d728e61abb182de2769c03231c9c60bc21

Fixup broken tests after #309
Small fixes for flake and unittests after merging #309 prematurely.

Note: as I was expecting the integration tests to be failing after #309,
I merged despite the big red cross warning of failing tests -- I whould
of course have checked that the basic tests _are_ passing.

scionlab/scion/config.py
@@ -27,7 +27,6 @@ from scionlab.defines import (
     DISPATCHER_PROM_PORT,
     CS_QUIC_PORT,
     CS_PROM_PORT,
-    SD_TCP_PORT,
     SD_PROM_PORT,
     SCION_CONFIG_DIR,
     SCION_VAR_DIR,

scionlab/tests/utils.py
@@ -503,7 +503,6 @@ def _check_tarball_etc_scion(testcase, tar, host):
         'beacon_policy.yaml',
         'certs',
         'keys',
-        'sd.toml',
         'topology.json',
     ]
     expected += [
@@ -544,7 +543,7 @@ def _check_tarball_info(testcase, tar, host):
           for _ in host.services.filter(type=Service.BW)]
 
     expected_services = br + cs + bw + [
-        "scion-daemon@sd.service",
+        "scion-daemon.service",
         "scion-dispatcher.service",
     ]
 